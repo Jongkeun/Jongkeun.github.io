cca49dfa8438c612b42d397ed8a2be4019421993
Jongkeun <jongkeun.kr@gmail.com>
2020-02-13 09:23:48 +0900

    Updates

---
+++ precache-manifest.b45442d82670bcf3e8845ee5a64f3ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a08e9517a88960e7725511681bb6813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcdc931e50d0342a642e",
+    "url": "/static/js/2.b46d5f65.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b46d5f65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cb6d8d16e4b8ffaf927",
+    "url": "/static/js/main.80f76553.chunk.js"
+  },
+  {
+    "revision": "9e8b32b8e24ed3538537",
+    "url": "/static/js/runtime-main.16444a05.js"
+  }
+]);
